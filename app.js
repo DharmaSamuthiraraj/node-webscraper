d9ad722edc7e5be13e897a0049ba7a2e0e8fadd4
Arumuga Samuthiraraj <asamuthiraraj@paypal.com>
2016-10-30 11:27:58 +0530

    web scraper first commit

---
+++ app.js
@@ -0,0 +1,44 @@
+var express = require('express');
+var request = require('request');
+var cheerio = require('cheerio');
+
+var app = express();
+var port = process.env.port || 3000;
+
+
+app.get('/scrape', function(req, res, next){
+    
+    var url = req.query.url;   
+    
+    if(url != undefined && url != '') {
+        request(url, function(error, response, html){
+           
+            if(!error){       
+                var $ = cheerio.load(html, {
+                    ignoreWhitespace: true,
+                    xmlMode: false,
+                    lowerCaseTags: false
+                });
+                var content = 'Output: <br><br>' + $('body').text();   
+                res.send(content); 
+                next();         
+            } 
+            else {
+               res.send(error); 
+               next();               
+            }      
+
+    	})	
+    } 
+    else {
+        res.send('<b>Partial content: </b> May be url param is missing or url is null');
+        next();     
+    }  
+
+});
+
+app.listen(port, function() {	
+  	console.log('Web Scraper app listening at ', port);
+})
+
+module.exports = app;
